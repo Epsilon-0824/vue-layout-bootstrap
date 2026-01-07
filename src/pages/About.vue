3dbf7eb190212a1dcf6f28a9ae67ec6614757b9e
Thanatorn Boontham <zyralolth@gmail.com>
2026-01-07 10:05:09 +0700

    initial commit

---
+++ src/pages/About.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>ABOUT PAGE</h1>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Contact.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>CONTACT PAGE</h1>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
